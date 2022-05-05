cd26b54ac1e083d36494f818e18528c3c5ccbebe
Marcus Liu <me@mail.lmarcus.onl>
2022-05-05 15:13:47 +0800

    feat(infra): 新增 passport 相關資料

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const helpers = require('./_helpers')
 const handlebars = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('./config/passport')
 
 const routes = require('./routes')
 
@@ -11,6 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
@@ -19,6 +22,10 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(methodOverride('_method'))
 

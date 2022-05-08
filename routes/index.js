cd93cdd68ae7e9f47a4efbd65cf1409aaa0e52a9
happygod119 <happ0983493655@gmail.com>
2022-05-08 09:23:09 +0800

    feat: 測試登入功能正常

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-// const { authenticated } = require('../middleware/auth')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const userController = require('../controller/userController')
+const tweetController = require('../controller/tweetsController.js')
 const exampleController = require('../controller/example-controller')
 
 // 使用者登入
@@ -12,8 +13,11 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 // 使用者註冊
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/logout', userController.logout)
+
+// 推文
+router.get('/tweets', authenticated, tweetController.getTweets)
+
 router.get('/', exampleController.indexPage)
 router.use('/', generalErrorHandler)
 

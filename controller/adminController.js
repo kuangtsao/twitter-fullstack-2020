556d8ce5207c1664781d37f39c9a09d6cee4f4ac
happygod119 <happ0983493655@gmail.com>
2022-05-09 13:38:58 +0800

    feat: add admin siginin

---
+++ controller/adminController.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs')
+const { Tweet, User, Like, Reply } = require('../models')
+const { Op } = require('sequelize')
+
+const adminController = {
+  signinPage: (req, res) => {
+    return res.render('admin/signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/admin/tweets')
+  },
+  getTweets: (req, res) => {
+    return res.render('admin/tweets')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/admin/signIn')
+  }
+}
+module.exports = adminController

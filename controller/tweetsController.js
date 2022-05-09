ade029514a6a80a675ce25d211f5c1e749a03750
Marcus Liu <me@mail.lmarcus.onl>
2022-05-10 02:56:59 +0800

    feat: 首頁初版 query 製作

--- controller/tweetsController.js
+++ controller/tweetsController.js
@@ -1,7 +1,20 @@
+const { Tweet, User, Like, Reply } = require('../models')
+
 const tweetsController = {
-  getTweets: (req, res) => {
+  getTweets: async (req, res, next) => {
     console.log(res.locals.user)
-    return res.render('index')
+    // 載入所有的 tweets (這裡先 query)
+    try {
+      const tweets = await Tweet.findAll({
+        include: User,
+        raw: true,
+        nest: true
+      })
+      console.log(tweets)
+      return res.render('index', { tweets })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = tweetsController

72bb48a1a94a345ff559b78e1942cdf365148494
Marcus Liu <me@mail.lmarcus.onl>
2022-05-10 21:54:41 +0800

    example controller 功成身退

--- controller/exampleController.js
+++
@@ -1,9 +0,0 @@
-// const { getUser } = require('../_helpers')
-
-const exampleController = {
-  indexPage: (req, res, next) => {
-    res.render('index')
-  }
-}
-
-module.exports = exampleController

2b24c5e980c2a358684a251f04f57dc09cda2918
Marcus Liu <me@mail.lmarcus.onl>
2022-05-10 17:17:40 +0800

    feat: top user controller 功能試做&載入資料

--- controller/tweetsController.js
+++ controller/tweetsController.js
@@ -14,7 +14,10 @@ const tweetsController = {
         raw: true,
         nest: true
       })
-      return res.render('index', { tweets })
+      const topUsers = await User.findAll({ raw: true })
+      console.log('--topUser--')
+      console.log(topUsers)
+      return res.render('index', { tweets, topUsers })
     } catch (err) {
       next(err)
     }

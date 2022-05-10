1f03039a72fe2acb88116a4c69255982ecb746e7
happygod119 <happ0983493655@gmail.com>
2022-05-10 19:05:34 +0800

    fix: login test bug

--- models/user.js
+++ models/user.js
@@ -17,11 +17,6 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'followerId',
         as: 'Followings'
       })
-      User.belongsToMany(models.Tweet, {
-        through: models.Like,
-        foreignKey: 'UserId',
-        as: 'LikedTweets'
-      })
     }
   }
   User.init(

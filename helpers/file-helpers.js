d7e66ef4fec1c6a98254e78378169b8ce2d237a2
happygod119 <happ0983493655@gmail.com>
2022-05-14 12:02:05 +0800

    feat : left and right layout

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -7,7 +7,8 @@ const localFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
     const fileName = `upload/${file.originalname}`
-    return fs.promises.readFile(file.path)
+    return fs.promises
+      .readFile(file.path)
       .then(data => fs.promises.writeFile(fileName, data))
       .then(() => resolve(`/${fileName}`))
       .catch(err => reject(err))
@@ -17,7 +18,8 @@ const localFileHandler = file => {
 const imgurFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
-    return imgur.uploadFile(file.path)
+    return imgur
+      .uploadFile(file.path)
       .then(img => {
         resolve(img?.link || null)
       })
